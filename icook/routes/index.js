6ff95819c6fc560ee8a79d16f113f82d60eb5848
yosoymizrra <jorgemizrain@gmail.com>
2019-03-31 10:19:26 -0600

    archivos iniciales de la api

--- icook/routes/index.js
+++ icook/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'iCook' });
 });
+
+
+/*router.get('/users/:id', (req, res)=>{
+  res.send('Un usuario');
+});*/
 
 module.exports = router;
