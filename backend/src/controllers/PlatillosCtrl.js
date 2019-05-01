ba47a58156a08c3c89d220799fbea7b2cfe37ec9
soymizra <jorgemizrain@gmail.com>
2019-05-01 12:53:40 -0600

    Restructuracion 3

--- backend/src/controllers/PlatillosCtrl.js
+++ backend/src/controllers/PlatillosCtrl.js
@@ -1,24 +1,4 @@
-const Sequelize = require('sequelize');
 
-const db = require('../config/db');
-
-const Platillos = db.define('Platillos',{
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    nombre: {
-        type: Sequelize.STRING(50)
-    },
-    imagen: Sequelize.STRING,
-    descripcion: Sequelize.STRING,
-    precio: Sequelize.DECIMAL,
-    idTipoDisponible: Sequelize.INTEGER,
-    url: Sequelize.STRING
-});
-
-module.exports = Platillos;
 
 
 
